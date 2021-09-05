fc7d902e9614dd408144ce29ace84412b67c9510
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-09-05 19:19:05 +0530

    importing all the packages

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express"
+import bp from 'body-parser'
+import morgan from 'morgan'
+
+const app = express()
+
+//we have to setup our middleware first then setup our routes and lastly we open our server ..
+
+
+ app.use(bp.urlencoded({extended: true})) //this is going to give if someone parse some data on the query string ,its gonna parse thst for us and it basically just make sure that we can parse the query string and whatever is in the URL pretty easily
+
+ app.use(bp.json())//this allows us to parse json body of a POST request,we can actually see it as a json and not like bits of chunks that we have to add together like we did in http server
+
+ app.use(morgan('dev'))//this gives the type of logging  that we want 
